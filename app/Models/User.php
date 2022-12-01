59ed742c6849b895eb8d75d20da5fbc11a91fed4
WidyaX1900 <tuderangga93@gmail.com>
2022-12-01 16:31:52 +0800

    Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function book()
+    {
+        return $this->belongsToMany(Book::class, 'book_rent', 'book_id', 'user_id');
+    }
 }
